385d55919712e8feb83679470abcba7948f12a3c
rutatiina <rutatiina@ruginem.org>
2022-07-31 18:15:10 +0300

    Updates & corrections

--- src/Services/GoodsReceivedService.php
+++ src/Services/GoodsReceivedService.php
@@ -42,11 +42,9 @@ class GoodsReceivedService
 
     public static function edit($id)
     {
-        $taxes = Tax::all()->keyBy('code');
 
         $txn = GoodsReceived::findOrFail($id);
-        $txn->load('contact', 'items.taxes');
-        $txn->setAppends(['taxes']);
+        $txn->load('contact', 'items');
 
         $attributes = $txn->toArray();
 
@@ -54,10 +52,8 @@ class GoodsReceivedService
 
         $attributes['_method'] = 'PATCH';
 
-        $attributes['contact']['currency'] = $attributes['contact']['currency_and_exchange_rate'];
-        $attributes['contact']['currencies'] = $attributes['contact']['currencies_and_exchange_rates'];
-
-        $attributes['taxes'] = json_decode('{}');
+        // $attributes['contact']['currency'] = $attributes['contact']['currency_and_exchange_rate'];
+        // $attributes['contact']['currencies'] = $attributes['contact']['currencies_and_exchange_rates'];
 
         foreach ($attributes['items'] as $key => $item)
         {
@@ -65,7 +61,7 @@ class GoodsReceivedService
                 'id' => $item['item_id'],
                 'name' => $item['name'],
                 'description' => $item['description'],
-                'rate' => $item['rate'],
+                'rate' => 0,
                 'tax_method' => 'inclusive',
                 'account_type' => null,
             ];
@@ -74,15 +70,8 @@ class GoodsReceivedService
             $attributes['items'][$key]['selectedTaxes'] = []; #required
             $attributes['items'][$key]['displayTotal'] = 0; #required
 
-            foreach ($item['taxes'] as $itemTax)
-            {
-                $attributes['items'][$key]['selectedTaxes'][] = $taxes[$itemTax['tax_code']];
-            }
-
-            $attributes['items'][$key]['rate'] = floatval($item['rate']);
             $attributes['items'][$key]['quantity'] = floatval($item['quantity']);
-            $attributes['items'][$key]['total'] = floatval($item['total']);
-            $attributes['items'][$key]['displayTotal'] = $item['total']; #required
+            $attributes['items'][$key]['displayTotal'] = 0; #required
         };
 
         return $attributes;
@@ -113,10 +102,8 @@ class GoodsReceivedService
             $Txn->contact_name = $data['contact_name'];
             $Txn->contact_address = $data['contact_address'];
             $Txn->reference = $data['reference'];
-            $Txn->total = $data['total'];
             $Txn->branch_id = $data['branch_id'];
             $Txn->store_id = $data['store_id'];
-            $Txn->status = $data['status'];
 
             $Txn->save();
 
@@ -192,9 +179,7 @@ class GoodsReceivedService
             }
 
             //Delete affected relations
-            $Txn->ledgers()->delete();
             $Txn->items()->delete();
-            $Txn->item_taxes()->delete();
             $Txn->comments()->delete();
 
             //reverse the account balances
@@ -208,22 +193,13 @@ class GoodsReceivedService
             $Txn->document_name = $data['document_name'];
             $Txn->number = $data['number'];
             $Txn->date = $data['date'];
-            $Txn->debit_financial_account_code = $data['debit_financial_account_code'];
             $Txn->contact_id = $data['contact_id'];
             $Txn->contact_name = $data['contact_name'];
             $Txn->contact_address = $data['contact_address'];
             $Txn->reference = $data['reference'];
-            $Txn->base_currency = $data['base_currency'];
-            $Txn->quote_currency = $data['quote_currency'];
-            $Txn->exchange_rate = $data['exchange_rate'];
-            $Txn->taxable_amount = $data['taxable_amount'];
-            $Txn->total = $data['total'];
             $Txn->branch_id = $data['branch_id'];
             $Txn->store_id = $data['store_id'];
-            $Txn->due_date = $data['due_date'];
             $Txn->contact_notes = $data['contact_notes'];
-            $Txn->terms_and_conditions = $data['terms_and_conditions'];
-            $Txn->status = $data['status'];
 
             $Txn->save();
 
@@ -290,9 +266,7 @@ class GoodsReceivedService
             }
 
             //Delete affected relations
-            $Txn->ledgers()->delete();
             $Txn->items()->delete();
-            $Txn->item_taxes()->delete();
             $Txn->comments()->delete();
 
             //reverse the account balances
