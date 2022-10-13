9a565832629fdfcb0da76da8b0a574d6629534ac
Gilbert Rutatiina <rutatiina@gmail.com>
2022-10-13 14:20:12 +0300

    cast canceled column to integer in model

--- src/Models/GoodsReceived.php
+++ src/Models/GoodsReceived.php
@@ -32,6 +32,7 @@ class GoodsReceived extends Model
 
     protected $casts = [
         'contact_id' => 'integer',
+        'canceled' => 'integer',
     ];
 
     protected $cloneable_relations = [
